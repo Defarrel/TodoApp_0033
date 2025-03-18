6af68270e1ae1c0195e9e5518c29fa36639a2c73
defarrel <Defarrel>
2025-03-18 23:22:16 +0700

    Membuat cupertino datepicker

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class TodoList extends StatefulWidget {
   const TodoList({super.key});
@@ -9,6 +10,37 @@ class TodoList extends StatefulWidget {
 
 class _TodoListState extends State<TodoList> {
   DateTime dateTime = DateTime.now();
+
+  void _showDatePicker(BuildContext context) {
+    showCupertinoModalPopup(
+      context: context,
+      builder:
+          (BuildContext context) => Container(
+            height: 300,
+            color: Colors.white,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 200,
+                  child: CupertinoDatePicker(
+                    mode: CupertinoDatePickerMode.dateAndTime,
+                    initialDateTime: dateTime,
+                    onDateTimeChanged: (DateTime newDateTime) {
+                      setState(() => dateTime = newDateTime);
+                    },
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                  child: Text('Select', style: TextStyle(color: Colors.white)),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ],
+            ),
+          ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +66,7 @@ class _TodoListState extends State<TodoList> {
                 IconButton(
                   color: Colors.blue,
                   icon: Icon(Icons.calendar_today),
-                  onPressed: () {}
+                  onPressed: () {},
                 ),
               ],
             ),

2beb3ee95ffcac5055d45c8dec5f864d5aef5146
defarrel <Defarrel>
2025-03-18 23:18:33 +0700

    Membuat taskdate dan icon date picker

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -8,12 +8,35 @@ class TodoList extends StatefulWidget {
 }
 
 class _TodoListState extends State<TodoList> {
-    DateTime dateTime = DateTime.now();
+  DateTime dateTime = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: const Text('Form Page')),
+      appBar: AppBar(title: Center(child: const Text('Form Page'))),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Task Date:', style: TextStyle(fontSize: 16)),
+                    Text(''),
+                  ],
+                ),
+                IconButton(
+                  color: Colors.blue,
+                  icon: Icon(Icons.calendar_today),
+                  onPressed: () {}
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
